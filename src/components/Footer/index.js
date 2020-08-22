db422dc236d8ca4cc0a5ec890c63123b4f519e84
Lorenalgm <lorena@proesc.com>
2020-08-22 02:38:20 -0300

    Criado componente de footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <footer>
+            <img src="" alt="Logo de mulheres na computação de norte a sul" />
+
+            <aside>
+                Realização
+                <img src="" alt="Meninas na computação" />
+                <img src="" alt="Gurias da computação" />
+            </aside>
+        </footer>
+    );
+}
+
+export default Footer;
