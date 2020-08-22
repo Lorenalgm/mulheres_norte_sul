7c0065ee84d6bc80739c39bf27fc64edde89e31b
Lorenalgm <lorena@proesc.com>
2020-08-22 15:01:19 -0300

    Configurado footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './styles.css';
 import logo from '../../assets/logomulheres.png';
+import logomeninas from '../../assets/logo.png';
+import logogurias from '../../assets/logogurias.png';
 
 function Footer(){
     return(
         <footer>
-            <img src={logo} alt="Logo de mulheres na computação de norte a sul" />
+            <img className="logo" src={logo} alt="Logo de mulheres na computação de norte a sul" />
 
             <aside>
                 Realização
-                <img src="" alt="Meninas na computação" />
-                <img src="" alt="Gurias da computação" />
+                <div className="projects">
+                    <img className="meninas" src={logomeninas} alt="Meninas na computação" />
+                    <img className="gurias" src={logogurias} alt="Gurias da computação" />
+                </div>
             </aside>
         </footer>
     );
