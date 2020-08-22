b70ae26ee7f01dd10371f49c18399306bcf789f2
Lorenalgm <lorena@proesc.com>
2020-08-22 02:14:36 -0300

    Criado header

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home(){
+    return(
+        <header>
+            <img src="" alt="Logo de mulheres na computação de norte a sul" />
+
+            <ul>
+                <li>Sobre</li>
+                <li>Agenda</li>
+            </ul>
+        </header>
+    );
+}
+
+export default Home;
