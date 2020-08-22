c9e829951f5bbf3d8fc6187bd990809e99c198f9
Lorenalgm <lorena@proesc.com>
2020-08-22 02:37:56 -0300

    Criado component de evento

---
+++ src/components/Event/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Event(props){
+    return(
+        <div className="event">
+            <div className="event-time">
+                <h1>{props.date}</h1>
+                <h3>{props.time}</h3>
+            </div>
+            <div className="event-content">
+                <div className="event-title">
+                    <h2>{props.type}</h2>
+                    <h1>{props.title}</h1>
+                </div>
+                <a className="button" href="#">Participar</a>
+            </div>
+        </div>
+    );
+}
+
+export default Event;
